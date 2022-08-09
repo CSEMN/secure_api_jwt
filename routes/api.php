d5d9241790d8d02911a4a03710d02382eac37158
Mahmoud Nasser <csemn.mail@gmail.com>
2022-08-09 15:45:58 +0200

    JWT auth
    implemented JWT auth controller with CRUD features.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api'], function($router) {
+    Route::post('/register', [\App\Http\Controllers\JWTController::class, 'register']);
+    Route::post('/login', [\App\Http\Controllers\JWTController::class, 'login']);
+    Route::post('/logout', [\App\Http\Controllers\JWTController::class, 'logout']);
+    Route::post('/refresh', [\App\Http\Controllers\JWTController::class, 'refresh']);
+    Route::post('/profile', [\App\Http\Controllers\JWTController::class, 'profile']);
+    Route::put('/update', [\App\Http\Controllers\JWTController::class, 'update']);
 });
