e6051756ddb5f4cecb61b1d5228542af7204f69c
Mahmoud Nasser <csemn.mail@gmail.com>
2022-08-10 17:51:44 +0200

    fix ouput
    fix response of user & product APIs

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -9,47 +10,60 @@ class ProductController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['jwt.verify','api'])->except(['index']);
-        $this->middleware(['CreatorOnly'])->only(['update','destroy']);
+        $this->middleware(['jwt.verify', 'api'])->except(['index']);
+        $this->middleware(['CreatorOnly'])->only(['update', 'destroy']);
     }
 
     public function index()
     {
-        return Product::all();
+        return ProductResource::collection(Product::all());
     }
 
     public function show(Product $product)
     {
-        return $product;
+        return new ProductResource($product);
     }
 
     public function store(Request $request)
     {
-        $fields = $request->validate([
-            'title'=>'required|string|min:3|max:100|unique:products,title',
-            'price'=>'required|numeric',
+        $request->validate([
+            'title' => 'required|string|min:3|max:100|unique:products,title',
+            'price' => 'required|numeric',
         ]);
-        $fields['user_id']=auth()->id();
-        $product = Product::create($fields);
+
+        $product = Product::create([
+            'title' => $request->title,
+            'price' => $request->price,
+            'user_id' => auth()->id(),
+        ]);
 
         return response()->json($product, 201);
     }
 
     public function update(Request $request, Product $product)
     {
-        $fields = $request->validate([
-            'title'=>'required|string|min:3|max:100|unique:products,title,' . $product->id,
-            'price'=>'required|numeric',
+        $request->validate([
+            'title' => 'sometimes|required|string|min:3|max:100|unique:products,title,' . $product->id,
+            'price' => 'sometimes|required|numeric',
         ]);
-        $product->update($fields);
+        if ($request->title)
+            $product->title = $request->title;
+        if ($request->price)
+            $product->price = $request->price;
 
-        return response()->json($product, 202);
+        if ($product->isDirty())
+            $product->save();
+
+        return new ProductResource($product);
     }
 
     public function destroy(Product $product)
     {
         $title = $product->title;
         $product->delete();
-        return response()->json(['status'=>"Product: $title, deleted Successfully"], 202);
+        return response()->json([
+            'status' => 200,
+            'message' => "Product: $title, deleted Successfully"
+        ], 202);
     }
 }
