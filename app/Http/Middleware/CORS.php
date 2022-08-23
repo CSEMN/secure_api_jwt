8680ee11de6a3dba84f426e53b0c036a5d363ff0
Mahmoud Nasser <csemn.mail@gmail.com>
2022-08-23 15:26:16 +0200

    Update CORS.php
    undo multi origins for security concerns

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -13,18 +13,15 @@ class CORS {
      */
     public function handle(Request $request, Closure $next)
     {
-        $allowedOrigins = ['https://secure-api-ui.herokuapp.com', 'http://localhost:4200'];
-        $origin = request()->headers->get('origin');
+        header('Access-Control-Allow-Origin:  https://secure-api-ui.herokuapp.com');
+//        header('Access-Control-Allow-Origin:  http://localhost:4200');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
+        header('Access-Control-Allow-Methods:  PUT, GET, HEAD, POST, DELETE, OPTIONS');
 
-        if(in_array($origin,$allowedOrigins)){
-            header('Access-Control-Allow-Origin',$origin);
-            header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
-            header('Access-Control-Allow-Methods:  PUT, GET, HEAD, POST, DELETE, OPTIONS');
-
-            if ($request->method() == "OPTIONS") {
-                return $next($request)->setStatusCode(200);
-            }
+        if ($request->method() == "OPTIONS") {
+            return $next($request)->setStatusCode(200);
         }
+
         return $next($request);
     }
 
